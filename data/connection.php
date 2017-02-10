ae7e942bf9f3801824ef1ed8847bcbbebe36fca6
Klvst3r <klvst3r@gmail.com>
2017-02-10 00:15:50 -0600

    Verify Connection

--- data/connection.php
+++ data/connection.php
@@ -5,5 +5,16 @@ class connection{
 		return mysqli_connect("localhost","dev","desarrollo");
 	}
 }
+/*
+* Verification of Database Connection
+* 
 
+$cnn = new connection();
+if($cnn->connect()){
+	echo "Connected";
+}
+else{
+	echo "Disconnected";
+}
+*/
 ?>
